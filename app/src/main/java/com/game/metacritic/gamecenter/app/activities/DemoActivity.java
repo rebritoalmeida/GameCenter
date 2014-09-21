a54ff4b8ebe0641c9d801b2ee70c5419da473075
Rui Almeida <rui.emanuel@gmail.com>
2014-09-21 23:01:11 +0100

    Shelf view demo implemented.

---
+++ app/src/main/java/com/game/metacritic/gamecenter/app/activities/DemoActivity.java
@@ -0,0 +1,50 @@
+package com.game.metacritic.gamecenter.app.activities;
+
+import com.game.metacritic.gamecenter.app.R;
+import com.game.metacritic.gamecenter.app.fragments.DemoFragment;
+import com.game.metacritic.gamecenter.app.fragments.EqualSpaceLayoutDemo;
+import com.game.metacritic.gamecenter.app.fragments.EqualSpaceLayoutDemo2;
+import com.game.metacritic.gamecenter.app.fragments.HorizontalListViewDemo;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.Preference.OnPreferenceClickListener;
+import android.preference.PreferenceActivity;
+
+public class DemoActivity extends PreferenceActivity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        addPreferencesFromResource(R.xml.menu);
+        
+        setupOnClick(findPreference("horizontallistview"), new Intent(this, HorizontalListViewDemo.class));
+        setupOnClick(findPreference("equalspace"), new Intent(this, EqualSpaceLayoutDemo.class));
+        setupOnClick(findPreference("equalspace2"), new Intent(this, EqualSpaceLayoutDemo2.class));
+        
+        findPreference("fragmentshell").setOnPreferenceClickListener(new OnPreferenceClickListener() {
+			
+			@Override
+			public boolean onPreferenceClick(Preference preference) {
+				Intent i = FragmentShellActivity.createIntent(getApplicationContext(), DemoFragment.class, null);
+				startActivity(i);
+				return true;
+			}
+		});
+    }
+
+    private void setupOnClick(Preference pref, final Intent intent) {
+        pref.setOnPreferenceClickListener(new OnPreferenceClickListener() {
+
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+                startActivity(intent);
+                return true;
+            }
+        });
+    }
+    
+
+}
---
+++ app/src/main/java/com/game/metacritic/gamecenter/app/activities/NavigationDelegate.java
@@ -0,0 +1,7 @@
+package com.game.metacritic.gamecenter.app.activities;
+
+public interface NavigationDelegate {
+
+	public void onNavigateBack();
+	
+}
